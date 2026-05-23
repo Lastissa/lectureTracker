d6a73eb6348a72e941aa0372a57e8b2485fb6368
lastissa <lastissa11@gmail.com>
2026-05-23 08:23:14 +0100

    new commit bug fix

--- api/views.py
+++ api/views.py
@@ -1140,33 +1140,33 @@ def login_json(request):
     email = request.data.get("email", None)
     password = request.data.get("password", "")
     user_type = request.data.get("user_type", None)
-    
+
     if email is None:
         return JsonResponse({"message": "email required"})
     if len(password) < 2:
         return JsonResponse({"message": "password required"})
     if user_type is None:
-    	return JsonResponse({"message" : "user_type required (new or old)"})
+        return JsonResponse({"message" : "user_type required (new or old)"})
     if user_type is None:
-    	return JsonResponse({"message" : "user_type required (new or old)"})
+        return JsonResponse({"message" : "user_type required (new or old)"})
     if user_type == "new" or user_type == "old":
-    	pass
+        pass
     else:
-    	return JsonResponse({"message" : "user_type required (new or old)"})
-    	#email and pass dey, validate 
-    person_exist = User.objects.filter(email__iexact = email)
+        return JsonResponse({"message" : "user_type required (new or old)"})
+        #email and pass dey, validate 
+    person_exist = User.objects.filter(email__iexact = email).first()
     if person_exist:
         if person_exist.check_password(password):
             #new user
             if user_type == "new":
-            	return JsonResponse({"message" : "email Taken"}, status = 409)
+                return JsonResponse({"message" : "email Taken"}, status = 409)
             elif user_type == "old":
-            	return JsonResponse({"message" : "success"}, status = 200)
+                return JsonResponse({"message" : "success", "username" : f"{person_exist}"}, status = 200)
         #wrong password
         if user_type == "new":
-        	return JsonResponse({"message" : "email Taken"}, status = 409)
+            return JsonResponse({"message" : "email Taken"}, status = 409)
         elif user_type == "old":
-        	return JsonResponse({"message": "incorrect password"}, status = 401)
+            return JsonResponse({"message": "incorrect password"}, status = 401)
     #no user
     return JsonResponse({"message": "user not found"}, status = 409)
 
