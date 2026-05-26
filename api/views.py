6b5bdf3a70d300a0d7ad2f3609ca044f743755d0
lastissa <lastissa11@gmail.com>
2026-05-26 07:00:42 +0100

    bug fix new

--- api/views.py
+++ api/views.py
@@ -1035,7 +1035,7 @@ def ai_response(request):
     
     
     
-    if os.getenv("local_db") is None:
+    if os.getenv("local_db") is not None:
         return JsonResponse({"message": "works"})
     else:
         import groq 
